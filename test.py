ae44c94d4ed32f953f714acac7f3a5e0cdbc3362
legos818 <79183065+legos818@users.noreply.github.com>
2022-11-02 11:54:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,79 @@
+import binascii
+import socket
+import sys
+from collections import OrderedDict
+
+# See https://web.archive.org/web/20180919041301/https://routley.io/tech/2017/12/28/hand-writing-dns-messages.html
+# See https://tools.ietf.org/html/rfc1035
+
+def send_udp_message(message, address, port):
+    """send_udp_message sends a message to UDP server
+    message should be a hexadecimal encoded string
+    """
+    message = message.replace(" ", "").replace("\n", "")
+    server_address = (address, port)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.sendto(binascii.unhexlify(message), server_address)
+        data, _ = sock.recvfrom(4096)
+    finally:
+        sock.close()
+    return binascii.hexlify(data).decode("utf-8")
+
+def build_message(type="A", address=""):
+    ID = 43690  # 16-bit identifier (0-65535) # 43690 equals 'aaaa'
+
+    QR = 0      # Query: 0, Response: 1     1bit
+    OPCODE = 0  # Standard query            4bit
+    AA = 0      # ?                         1bit
+    TC = 0      # Message is truncated?     1bit
+    RD = 1      # Recursion?                1bit
+    RA = 0      # ?                         1bit
+    Z = 0       # ?                         3bit
+    RCODE = 0   # ?                         4bit
+
+    query_params = str(QR)
+    query_params += str(OPCODE).zfill(4)
+    query_params += str(AA) + str(TC) + str(RD) + str(RA)
+    query_params += str(Z).zfill(3)
+    query_params += str(RCODE).zfill(4)
+    query_params = "{:04x}".format(int(query_params, 2))
+
+    QDCOUNT = 1 # Number of questions           4bit
+    ANCOUNT = 0 # Number of answers             4bit
+    NSCOUNT = 0 # Number of authority records   4bit
+    ARCOUNT = 0 # Number of additional records  4bit
+
+    message = ""
+    message += "{:04x}".format(ID)
+    message += query_params
+    message += "{:04x}".format(QDCOUNT)
+    message += "{:04x}".format(ANCOUNT)
+    message += "{:04x}".format(NSCOUNT)
+    message += "{:04x}".format(ARCOUNT)
+
+    # QNAME is url split up by '.', preceded by int indicating length of part
+    addr_parts = address.split(".")
+    for part in addr_parts:
+        addr_len = "{:02x}".format(len(part))
+        addr_part = binascii.hexlify(part.encode())
+        message += addr_len
+        message += addr_part.decode()
+
+    message += "00" # Terminating bit for QNAME
+
+    # Type of request
+    QTYPE = get_type(type)
+    message += QTYPE
+
+    # Class for lookup. 1 is Internet
+    QCLASS = 1
+    message += "{:04x}".format(QCLASS)
+
+    return message
+
+
 def decode_message(message):    
     res = []
     
@@ -85,3 +161,59 @@ def decode_message(message):
                 res.append("RDDATA decoded (result): " + RDDATA_decoded + "\n")
 
     return "\n".join(res)
+
+def get_type(type):
+    types = [
+        "ERROR", # type 0 does not exist
+        "A",
+        "NS",
+        "MD",
+        "MF",
+        "CNAME",
+        "SOA",
+        "MB",
+        "MG",
+        "MR",
+        "NULL",
+        "WKS",
+        "PTS",
+        "HINFO",
+        "MINFO",
+        "MX",
+        "TXT"
+    ]
+
+    return "{:04x}".format(types.index(type)) if isinstance(type, str) else types[type]
+
+def parse_parts(message, start, parts):
+    part_start = start + 2
+    part_len = message[start:part_start]
+    
+    if len(part_len) == 0:
+        return parts
+    
+    part_end = part_start + (int(part_len, 16) * 2)
+    parts.append(message[part_start:part_end])
+
+    if message[part_end:part_end + 2] == "00" or part_end > len(message):
+        return parts
+    else:
+        return parse_parts(message, part_end, parts)
+
+
+# Usage: python3 raw-dns-req.py github.com
+
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    url = "github.com"
+
+# See get_type function for other possibilities for first argument
+message = build_message("A", "tmz.com") 
+print("Request:\n" + message)
+print("\nRequest (decoded):" + decode_message(message))
+
+# second argument is external DNS server, third argument is port
+response = send_udp_message(message, "91.245.229.1", 53)
+print("\nResponse:\n" + response)
+print("\nResponse (decoded):" + decode_message(response))
